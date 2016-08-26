99f34621f681d0f7f8ffe4bdbad7341f018202f8
pengjinfeng <peng_jinfeng@163.com>
2016-08-26 13:20:42 +0800

    JFBackground
    iOS 后台无限时常下载

---
+++ JFBackground/JFBackground/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JFBackground
+//
+//  Created by apple on 16/6/4.
+//  Copyright © 2016年 pengjf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
